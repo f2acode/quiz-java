d689ebedf6de618e628025edbd2042a6013085f1
Felipe Augusto de Almeida <Felipe Augusto de Almeida>
2017-02-28 14:04:52 -0300

    Correcting the loop inside main

--- helloworld.cs
+++ helloworld.cs
@@ -5,8 +5,10 @@ public class Hello3
    public static void Main(string[] args)
    {
       Console.WriteLine("Hello, World!");
-      while(true){
-      	Console.WriteLine("Teste");
+	int i = 0;
+      while(i < 6){
+      	Console.WriteLine("Teste"+i);
+	i++;
       };
    }
 }
